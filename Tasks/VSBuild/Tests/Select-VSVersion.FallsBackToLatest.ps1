4b14a52ff667d49b0c91ba83aced42363136298d
Eric Sciple <ersciple@microsoft.com>
2017-04-27 02:41:15 -0400

    .

--- Tasks/VSBuild/Tests/Select-VSVersion.FallsBackToLatest.ps1
+++ Tasks/VSBuild/Tests/Select-VSVersion.FallsBackToLatest.ps1
@@ -5,7 +5,8 @@ param()
 . $PSScriptRoot\..\..\..\Tests\lib\Initialize-Test.ps1
 . $PSScriptRoot\..\Select-VSVersion.ps1
 Register-Mock Write-Warning
-Register-Mock Get-VSPath { "Some location" } -- -Version '15.0'
+Register-Mock Get-VSPath { "Some location" } -- -Version '14.0'
+throw 'use MSBuildHelpers/Select-MSBuildVersion tests as a go-by to for the Select-VSVersion tests'
 
 # Act.
 $actual = Select-VSVersion -PreferredVersion '16.0'
